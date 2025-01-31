1289bbbecccfffb975ca6b0b8d0d82353873ab92
Jinapetto <144988328+Jinapetto@users.noreply.github.com>
2025-01-31 15:49:39 +0900

    Create xor128.hpp

---
+++ src/Huristric/xor128.hpp
@@ -0,0 +1,7 @@
+unsigned long xor128(){
+	static unsigned long x = 123456789,y = 362436069,z = 541288629,w = 88675123;
+	unsigned long t;
+	t = (x^(x<<11));
+	x = y;y = z;z = w;
+	return(w = (w^(w>>19))^(t^(t>>8)));
+}
